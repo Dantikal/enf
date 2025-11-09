a0ee97e3453596db921125de4d019e8b1593e108
Danchik <danisaifudinov@gmail.com>
2025-11-09 15:49:47 +0600

    add templates

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
-app_name = '.mian'
+app_name = 'main'
+
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('catalaog/', views.CatalogView.as_view(), name='catalog_all'),
-    path('catalog/<slug:category_slug>/', views.CatalohgView.as_view(), name='catalog'),
+    path('catalog/', views.CatalogView.as_view(), name='catalog_all'),
+    path('catalog/<slug:category_slug>/', views.CatalogView.as_view(), name='catalog'),
     path('product/<slug:slug>', views.ProductDetailView.as_view(), name='product_detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
